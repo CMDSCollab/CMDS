c02dbbb43a57c9e2b0a5e7fd6ca558d37803daac
HowLowHello <cc54321@163.com>
2022-05-01 20:59:05 +0800

    Update TestScript.cs

--- CMDS-GitHub/Assets/TestScript.cs
+++ CMDS-GitHub/Assets/TestScript.cs
@@ -7,13 +7,14 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("hhhhhh");
- 
+        //Debug.Log("hhhhhh");
+
+        Debug.Log("12345");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
