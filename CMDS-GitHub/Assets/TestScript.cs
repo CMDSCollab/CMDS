5ee3d875f7a88ce1569001c97aacc0789a867ecf
Jahaha1 <ab2805281@gmail.com>
2022-05-01 21:11:46 +0800

    Update TestScript.cs

--- CMDS-GitHub/Assets/TestScript.cs
+++ CMDS-GitHub/Assets/TestScript.cs
@@ -7,11 +7,17 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         //Debug.Log("hhhhhh");
 
         Debug.Log("12345");
         Debug.Log("asdasaaaa");
         Debug.Log("saadsadsadsa");
+=======
+        Debug.Log("hhhhhh");
+        Debug.Log("dasdasda");
+        Debug.Log("saaaa");
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
